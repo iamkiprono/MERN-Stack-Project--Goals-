69a9902cd003482f3f2c70b0ab34186d34ff87ea
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-04-04 13:19:42 +0300

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/goals", require("./routes/goalRoutes"))
+app.use("/api/goals", require("./routes/goalRoutes"));
 
-app.listen(port, () => console.log(`Server at ${port}`));
+app.listen(port, () => console.log(`Server started at ${port}`));
